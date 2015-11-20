93fe4c5019905dbe38e1f883baf92f88decf705a
Thunder <thunder@uoregon.edu>
2015-11-19 21:04:15 -0800

    Add django project

---
+++ HackerFeed/feeds/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('index.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+        
