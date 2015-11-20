8e42d35135936023590a08d35c3c2400616ff8bb
Thunder <thunder@uoregon.edu>
2015-11-19 21:22:34 -0800

    All unit tests passing

--- HackerFeed/feeds/tests.py
+++ HackerFeed/feeds/tests.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from feeds.views import home_page
 
 class HomePageTest(TestCase):
 
