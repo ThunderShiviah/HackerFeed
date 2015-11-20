d791fb23b17e2ccefde296805e99e4279e3106fb
Thunder <thunder@uoregon.edu>
2015-11-19 20:36:09 -0800

    Add user story to functional test

--- HackerFeed/functional_tests.py
+++ HackerFeed/functional_tests.py
@@ -12,7 +12,30 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # I navigate to the site.
         self.browser.get('http://localhost:8000')
+
+        # I'm greeted by HackerFeed in the title bar
+        # and in the header.
+
+        # I look down to see a list of articles.
+        
+        # Each article has the title
+
+        # and the url.
+
+        # The articles are a mix of many blogs.
+
+        # When I hover over the article, the url becomes underlined.
+
+        # When I click on the article, the article website opens in a new window.
+        
+        # The link is then marked as visited.
+
+        # I can then press an archive button which hides that story from view and replaces it with a new one.
+
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':  
